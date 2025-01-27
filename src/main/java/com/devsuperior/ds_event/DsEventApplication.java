0b8f4cb53c02c824c778889af570f0b86e7de492
Hennan H. Falcão <hennan.falcao@cs.unipe.edu.br>
2025-01-26 22:10:11 -0300

    Desafio concluído

---
+++ src/main/java/com/devsuperior/ds_event/DsEventApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ds_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/ds_event/DsEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ds_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
